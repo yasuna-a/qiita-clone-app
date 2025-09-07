50f4bb275e5b4229e9446db9303ce7b012e00acc
yasuna-a <yas613a@gmail.com>
2025-09-07 13:38:59 +0900

    Add 最低限のベース追加

---
+++ app/features/home/index.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui";
+
+export function Home() {
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  );
+}
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,5 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { Home as Presenter } from "@/features/home";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,5 +9,5 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <Presenter />;
 }
